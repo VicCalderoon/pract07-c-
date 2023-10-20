b0ee82dee691f4fb7b68b7fbdf0a862d12eb3b9d
VicCalderoon <146136453+VicCalderoon@users.noreply.github.com>
2023-10-19 21:17:08 -0700

    Agregar archivos de proyecto.

---
+++ pract07 c#/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        const int capacidad = 10;
+        int[] elementos = new int[capacidad];
+        int tope = -1;
+        Console.WriteLine("teclea elementos de la pila (termina con -1)");
+        int x = 0;
+        const int CLAVE = -1;
+        while (x != CLAVE)
+        {
+            try
+            {
+                x = int.Parse(Console.ReadLine());
+                if (tope < capacidad - 1)
+                {
+                    tope++;
+                    elementos[tope] = x;
+                }
+                else
+                {
+                    Console.WriteLine("excepcion: Pila llena");
+                    break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("excepcion: ENTRADA NO VALIDA");
+
+            }
+        }
+        if (tope >= 0)
+        {
+            Console.WriteLine("elementos de la pila:  ");
+            while (tope >= 0)
+            {
+                x = elementos[tope];
+                tope--;
+                Console.WriteLine(x + "");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Pila vacia ");
+        }
+    }
+
+}
